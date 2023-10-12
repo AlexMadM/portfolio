ef9089c67e8ad21aa96b92fd0349901cb81486b3
Alex <alex.alexandrov.1988@gmail.com>
2023-10-12 23:46:44 +0300

    Main

--- src/layout/section/main/Main.tsx
+++ src/layout/section/main/Main.tsx
@@ -1,23 +1,38 @@
 import React from 'react';
-import photo from '../../../assets/img/proj2.webp'
+import photo from '../../../assets/img/blogging 1.png'
 import styled from "styled-components";
+import {FlexWrapper} from "../../../components/FlexWrapper";
 const Main = () => {
     return (
-        <div>
-            <div>
-                <span>Hi</span>
-                <h2>Hi am Alexander</h2>
-                <h1>I am web Developer</h1>
-            </div>
-            <Photo src={photo}/>
-        </div>
+        <StyledMain>
+          <FlexWrapper align={'center'} justify={'space-around'}>
+              <div>
+                  <span>Hi</span>
+                  <Name>I am Alexander</Name>
+                  <MainTitle>I  web Developer</MainTitle>
+              </div>
+              <Photo src={photo}/>
+          </FlexWrapper>
+        </StyledMain>
     );
 };
+
+const StyledMain =styled.div`
+min-height: 100vh;
+  background-color: aquamarine;
+`
 
 const Photo =styled.img`
 width: 350px;
   height: 450px;
   object-fit: cover;
+`
+
+const MainTitle = styled.h1`
+
+`
+
+const Name =styled.h2`
 `
 
 export default Main;
