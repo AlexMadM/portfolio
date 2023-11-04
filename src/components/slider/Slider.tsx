a3d2ef43917173788d17be8680f65360028f808a
Alex <alex.alexandrov.1988@gmail.com>
2023-11-04 21:17:19 +0300

    slider

--- src/components/slider/Slider.tsx
+++ src/components/slider/Slider.tsx
@@ -1,30 +1,56 @@
 import React from 'react';
+import AliceCarousel from 'react-alice-carousel';
+import 'react-alice-carousel/lib/alice-carousel.css';
 import styled from "styled-components";
-import {FlexWrapper} from "../FlexWrapper";
 import {theme} from "../../styles/Theme";
+import './../../styles/slider.css'
 
-const Slider = () => {
+type SlidePropsType ={
+    text: string
+    userName:string
+}
+
+const Slid =(props:SlidePropsType)=> {
     return (
-        <StyledSlider>
-            <FlexWrapper><Slide>
-                <Text>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut
-                    labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing
-                    elit.</Text>
-                <Name>@ivan ivanow</Name>
-            </Slide>
-            </FlexWrapper>
-            <Pagination>
-                <span></span>
-                <span className={'active'}></span>
-                <span></span>
-            </Pagination>
-        </StyledSlider>
-    );
-};
+        <Slide>
+            <Text>{props.text}</Text>
+            <Name>{props.userName}</Name>
+        </Slide>
+    )
+}
+
+const items = [
+    <Slid userName={'ivan ivanow'} text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut\n' +
+        '                labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing\n' +
+        '                elit.'}/>,
+    <Slid userName={'ivan ivanow'} text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut\n' +
+        '                labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing\n' +
+        '                elit.'}/>,
+    <Slid userName={'ivan ivanow'} text={'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut\n' +
+        '                labore et dolore magna aliqua Ut enim. Lorem ipsum dolor sit amet, consectetur adipisicing\n' +
+        '                elit.'}/>
+];
+
+export const Slider = () => (
+   <StyledSlider>
+       <AliceCarousel
+           mouseTracking
+           items={items}
+
+       />
+
+   </StyledSlider>
+
+);
+
+
+
+
 
 const StyledSlider = styled.div`
   border: 1px solid red;
   max-width: 500px;
+  width: 100%;
   display: flex;
   flex-direction: column;
 
